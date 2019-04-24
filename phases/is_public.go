4797c315bd7fdd3786c8b432cab8318423a2480b
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-04-24 14:07:33 +0200

    choose privacy (happy path)

--- phases/is_public.go
+++ phases/is_public.go
@@ -1,9 +1,47 @@
 package phases
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type visibilityOption struct{
+	Name string
+	IsPublic bool
+}
 
 // IsPublic ...
 func IsPublic() (bool, error) {
-	fmt.Println("SetIsPublic")
-	return false, nil
+	options := []visibilityOption{
+		visibilityOption{"Private", false},
+		visibilityOption{"Public", true},
+	}
+
+	fmt.Println("SET THE PRIVACY OF THE APP")
+	for i, opt := range options {
+		fmt.Printf("%d) %s", i + 1, opt.Name)
+		fmt.Println()
+	}
+	fmt.Print("CHOOSE THE VISIBILITY: ")
+
+	// scan for input
+	reader := bufio.NewReader(os.Stdin)
+
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		// todo
+	}
+	
+	choice, err := strconv.Atoi(strings.Trim(input, "\n"))
+	if err != nil {
+		// todo
+	}
+
+	fmt.Printf("your choice was %s", options[choice - 1].Name)
+	fmt.Println()
+
+	return options[choice - 1].IsPublic, nil
 }
