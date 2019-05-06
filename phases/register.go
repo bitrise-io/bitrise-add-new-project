88daf6d346da65a1386b2a99859f101b69f8ed6a
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-05-06 18:24:01 +0200

    webhook registration error handling

--- phases/register.go
+++ phases/register.go
@@ -25,41 +25,46 @@ func registerWebhook(appSlug string, apiToken string) error {
 	url := fmt.Sprintf("%s/app/%s/register-webhook.json", baseURL, appSlug)
 	request, err := http.NewRequest(http.MethodPost, url, nil)
 	if err != nil {
-		// todo
+		return fmt.Errorf("create POST %s request: %s", url, err)
 	}
 
 	request.Header.Add("Authorization", "token "+ apiToken)
 	resp, err := http.DefaultClient.Do(request)
 	if err != nil {
-		// todo
+		fmt.Errorf("send POST %s request: %s", url, err)
 	}
 
 	if resp.StatusCode == 400 {
 		log.Errorf("Error registering webhook")
 		log.Warnf("Make sure you have the required access rights to the repository and that you enabled git provider integration jfor your Bitrise account!")
+		log.Warnf("Please fix your configuration and hit enter to try again!")
 		
 		reader := bufio.NewReader(os.Stdin)
 
 		for {
 			_, _ = reader.ReadString('\n')
-			if err := registerWebhook(appSlug, apiToken); err == nil {
-				return nil
+			if err := registerWebhook(appSlug, apiToken); err != nil {
+				log.Errorf("Error registering webhook: %s", err)
+				log.Warnf("Please fix your configuration and hit enter to try again!")
+				continue
 			}
+			return nil
 		}
 	}
 
 	if resp.StatusCode != 200 {
 		data, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			// todo
+			return fmt.Errorf("read create webhook response: %s", err)
 		}
 		var m map[string]string
-
+		
 		if err := json.Unmarshal(data, &m); err != nil {
-			// todo
+			return fmt.Errorf("unmarshal create webhook response: %s", err)
 		}
 
 		log.Errorf("Error registering webhook: %s %s", resp.Status, m["error_msg"])
+		return fmt.Errorf("server error registering webhook: %s %s", resp.Status, m["error_msg"])
 	}
 
 	log.Successf("Webhook registered")
