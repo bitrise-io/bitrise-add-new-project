21a50f888863dfb737e8f31cf86a9bc13952717b
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-05-07 16:06:49 +0200

    webhook phase

--- phases/webhook.go
+++ phases/webhook.go
@@ -1,9 +1,25 @@
 package phases
 
-import "fmt"
+import "github.com/bitrise-io/go-utils/log"
 
 // AddWebhook ...
 func AddWebhook() (bool, error) {
-	fmt.Println("SetAddWebhook")
-	return false, nil
+	var registerWebhook bool
+
+	log.Printf("WEBHOOK SETUP")
+	log.Printf("To have Bitrise automatically start a build every time you push code into your repository you can set up a webhook at your code hosting service which will automatically trigger a build on Bitrise with the code you push to your repository.")
+	log.Printf("We can automatically register a Webhook for you if you have administrator rights for this repository.")
+
+	(&option{
+		title:        "Would you like us to register a webhok fro you?",
+		valueOptions: []string{"yes", "no"},
+		action: func(answer string) *option {
+			if answer == "yes" {
+				registerWebhook = true
+			}
+			return nil
+		},
+	}).run()
+
+	return registerWebhook, nil
 }
