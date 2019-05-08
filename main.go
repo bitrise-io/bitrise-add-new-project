46ea27d8ebe481126ad13811823fec1425154b83
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-05-08 11:17:44 +0200

    linter fixes

--- main.go
+++ main.go
@@ -1,8 +1,5 @@
 package main
 
-// import  "github.com/bitrise-io/bitrise-add-new-project/cmd"
-import "github.com/bitrise-io/bitrise-add-new-project/phases"
-
 func main() {
-	phases.Stack("")
+
 }
