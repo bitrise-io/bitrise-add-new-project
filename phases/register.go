9e92bc1027744b44ea46ba037502ff7ce62d3f45
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-05-15 10:17:59 +0200

    refactor: use built-in constants

--- phases/register.go
+++ phases/register.go
@@ -45,7 +45,7 @@ func registerWebhook(appSlug string, apiToken string) error {
 		}
 	}()
 
-	if resp.StatusCode == 400 {
+	if resp.StatusCode == http.StatusBadRequest {
 		log.Errorf("Error registering webhook")
 		log.Warnf("Make sure you have the required access rights to the repository and that you enabled git provider integration jfor your Bitrise account!")
 		log.Warnf("Please fix your configuration and hit enter to try again!")
@@ -70,7 +70,7 @@ func registerWebhook(appSlug string, apiToken string) error {
 		}
 	}
 
-	if resp.StatusCode != 200 {
+	if resp.StatusCode != http.StatusOK {
 		data, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			return fmt.Errorf("read create webhook response: %s", err)
