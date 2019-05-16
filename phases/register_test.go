6dd8eb98440b13f22a53712d409d30ca62ce0a99
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-05-16 13:34:39 +0200

    pr fix: no commented code

--- phases/register_test.go
+++ phases/register_test.go
@@ -3,7 +3,6 @@ package phases
 import (
 	"net/http"
 	"net/http/httptest"
-	// "os"
 	"testing"
 )
 
@@ -24,32 +23,6 @@ func TestRegisterWebhook(t *testing.T) {
 	if err := registerWebhook(appSlug, apiToken); err != nil {
 		t.Fatalf("err should be nil instead of %s", err)
 	}
-
-	// retriable error case
-	// if os.Stdin, err = os.Create("testfile"); err != nil {
-	// 	t.Fatalf("reset stdin to readable file: %s", err)
-
-	// }
-	// if _, err := os.Stdin.WriteString("\n"); err != nil {
-	// 	t.Fatalf("write newline to stdin: %s", err)
-	// }
-
-	// testServer400 := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-	// 	res.WriteHeader(400)
-	// }))
-	// defer func() { testServer400.Close() }()
-
-	// baseURL = testServer400.URL
-
-	// err = registerWebhook(appSlug, apiToken)
-
-	// if err == nil {
-	// 	t.Fatalf("err should object instead of nil")
-	// }
-
-	// if webhookAttemptCount != webhookAttemptMax {
-	// 	t.Fatalf("exit before webhook max attempts reached")
-	// }
 
 	// fatal error
 	testServer500 := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
