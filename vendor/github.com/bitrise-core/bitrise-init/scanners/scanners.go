25904209f66e4af4e19857651963c45e8e519122
Laszlo Pusok <laszlo.pusok@bitrise.io>
2019-05-21 10:56:50 +0200

    dep update

---
+++ vendor/github.com/bitrise-core/bitrise-init/scanners/scanners.go
@@ -0,0 +1,100 @@
+package scanners
+
+import (
+	"github.com/bitrise-core/bitrise-init/models"
+	"github.com/bitrise-core/bitrise-init/scanners/android"
+	"github.com/bitrise-core/bitrise-init/scanners/cordova"
+	"github.com/bitrise-core/bitrise-init/scanners/fastlane"
+	"github.com/bitrise-core/bitrise-init/scanners/ionic"
+	"github.com/bitrise-core/bitrise-init/scanners/ios"
+	"github.com/bitrise-core/bitrise-init/scanners/macos"
+	"github.com/bitrise-core/bitrise-init/scanners/reactnative"
+	"github.com/bitrise-core/bitrise-init/scanners/xamarin"
+	"github.com/bitrise-core/bitrise-init/steps"
+	"gopkg.in/yaml.v2"
+)
+
+// ScannerInterface ...
+type ScannerInterface interface {
+	// The name of the scanner is used for logging and
+	// to store the scanner outputs, like warnings, options and configs.
+	// The outputs are stored in a map[NAME]OUTPUT, like: warningMap[ios]warnings, optionsMap[android]options, configMap[xamarin]configs, ...,
+	// this means, that the SCANNER NAME HAS TO BE UNIQUE.
+	// Returns:
+	// - the name of the scanner
+	Name() string
+
+	// Should implement as minimal logic as possible to determin if searchDir contains the - in question - platform or not.
+	// Inouts:
+	// - searchDir: the directory where the project to scann exists.
+	// Returns:
+	// - platform detected
+	// - error if (if any)
+	DetectPlatform(string) (bool, error)
+
+	// ExcludedScannerNames is used to mark, which scanners should be excluded, if the current scanner detects platform.
+	ExcludedScannerNames() []string
+
+	// OptionModel is the model, used to store the available configuration combintaions.
+	// It defines option branches which leads different bitrise configurations.
+	// Each branch should define a complete and valid options to build the final bitrise config model.
+	// Every OptionModel branch's last options has to be the key of the workflow (in the BitriseConfigMap), which will fulfilled with the selected options.
+	// Returns:
+	// - OptionModel
+	// - Warnings (if any)
+	// - error if (if any)
+	Options() (models.OptionModel, models.Warnings, error)
+
+	// Returns:
+	// - default options for the platform.
+	DefaultOptions() models.OptionModel
+
+	// BitriseConfigMap's each element is a bitrise config template which will fulfilled with the user selected options.
+	// Every config's key should be the last option one of the OptionModel branches.
+	// Returns:
+	// - platform BitriseConfigMap
+	Configs() (models.BitriseConfigMap, error)
+
+	// Returns:
+	// - platform default BitriseConfigMap
+	DefaultConfigs() (models.BitriseConfigMap, error)
+}
+
+// ActiveScanners ...
+var ActiveScanners = []ScannerInterface{
+	reactnative.NewScanner(),
+	ionic.NewScanner(),
+	cordova.NewScanner(),
+	ios.NewScanner(),
+	macos.NewScanner(),
+	android.NewScanner(),
+	xamarin.NewScanner(),
+	fastlane.NewScanner(),
+}
+
+// CustomProjectType ...
+const CustomProjectType = "other"
+
+// CustomConfigName ...
+const CustomConfigName = "other-config"
+
+// CustomConfig ...
+func CustomConfig() (models.BitriseConfigMap, error) {
+	configBuilder := models.NewDefaultConfigBuilder()
+	configBuilder.AppendStepListItemsTo(models.PrimaryWorkflowID, steps.DefaultPrepareStepList(false)...)
+	configBuilder.AppendStepListItemsTo(models.PrimaryWorkflowID, steps.DefaultDeployStepList(false)...)
+
+	config, err := configBuilder.Generate(CustomProjectType)
+	if err != nil {
+		return models.BitriseConfigMap{}, err
+	}
+
+	data, err := yaml.Marshal(config)
+	if err != nil {
+		return models.BitriseConfigMap{}, err
+	}
+
+	return models.BitriseConfigMap{
+		CustomConfigName: string(data),
+	}, nil
+}
---
+++ vendor/github.com/bitrise-core/bitrise-init/steps/const.go
@@ -0,0 +1,192 @@
+package steps
+
+const (
+	// ActivateSSHKeyID ...
+	ActivateSSHKeyID = "activate-ssh-key"
+	// ActivateSSHKeyVersion ...
+	ActivateSSHKeyVersion = "3.1.1"
+)
+
+const (
+	// GitCloneID ...
+	GitCloneID = "git-clone"
+	// GitCloneVersion ...
+	GitCloneVersion = "3.6.2"
+)
+
+const (
+	// CachePullID ...
+	CachePullID = "cache-pull"
+	// CachePullVersion ...
+	CachePullVersion = "2.0.1"
+)
+
+const (
+	// CachePushID ...
+	CachePushID = "cache-push"
+	// CachePushVersion ...
+	CachePushVersion = "2.0.3"
+)
+
+const (
+	// CertificateAndProfileInstallerID ...
+	CertificateAndProfileInstallerID = "certificate-and-profile-installer"
+	// CertificateAndProfileInstallerVersion ...
+	CertificateAndProfileInstallerVersion = "1.9.2"
+)
+
+const (
+	// DeployToBitriseIoID ...
+	DeployToBitriseIoID = "deploy-to-bitrise-io"
+	// DeployToBitriseIoVersion ...
+	DeployToBitriseIoVersion = "1.3.9"
+)
+
+const (
+	// ScriptID ...
+	ScriptID = "script"
+	// ScriptVersion ...
+	ScriptVersion = "1.1.5"
+	// ScriptDefaultTitle ...
+	ScriptDefaultTitle = "Do anything with Script step"
+)
+
+const (
+	// InstallMissingAndroidToolsID ...
+	InstallMissingAndroidToolsID = "install-missing-android-tools"
+	// InstallMissingAndroidToolsVersion ...
+	InstallMissingAndroidToolsVersion = "2.0.6"
+)
+
+const (
+	// GradleRunnerID ...
+	GradleRunnerID = "gradle-runner"
+	// GradleRunnerVersion ...
+	GradleRunnerVersion = "1.8.0"
+)
+
+const (
+	// FastlaneID ...
+	FastlaneID = "fastlane"
+	// FastlaneVersion ...
+	FastlaneVersion = "2.3.9"
+)
+
+const (
+	// CocoapodsInstallID ...
+	CocoapodsInstallID = "cocoapods-install"
+	// CocoapodsInstallVersion ...
+	CocoapodsInstallVersion = "1.7.2"
+)
+
+const (
+	// CarthageID ...
+	CarthageID = "carthage"
+	// CarthageVersion ...
+	CarthageVersion = "3.1.3"
+)
+
+const (
+	// RecreateUserSchemesID ...
+	RecreateUserSchemesID = "recreate-user-schemes"
+	// RecreateUserSchemesVersion ...
+	RecreateUserSchemesVersion = "1.0.2"
+)
+
+const (
+	// XcodeArchiveID ...
+	XcodeArchiveID = "xcode-archive"
+	// XcodeArchiveVersion ...
+	XcodeArchiveVersion = "2.4.2"
+)
+
+const (
+	// XcodeTestID ...
+	XcodeTestID = "xcode-test"
+	// XcodeTestVersion ...
+	XcodeTestVersion = "1.18.8"
+)
+
+const (
+	// XamarinUserManagementID ...
+	XamarinUserManagementID = "xamarin-user-management"
+	// XamarinUserManagementVersion ...
+	XamarinUserManagementVersion = "1.1.0"
+)
+
+const (
+	// NugetRestoreID ...
+	NugetRestoreID = "nuget-restore"
+	// NugetRestoreVersion ...
+	NugetRestoreVersion = "1.0.5"
+)
+
+const (
+	// XamarinComponentsRestoreID ...
+	XamarinComponentsRestoreID = "xamarin-components-restore"
+	// XamarinComponentsRestoreVersion ...
+	XamarinComponentsRestoreVersion = "0.9.0"
+)
+
+const (
+	// XamarinArchiveID ...
+	XamarinArchiveID = "xamarin-archive"
+	// XamarinArchiveVersion ...
+	XamarinArchiveVersion = "1.5.0"
+)
+
+const (
+	// XcodeArchiveMacID ...
+	XcodeArchiveMacID = "xcode-archive-mac"
+	// XcodeArchiveMacVersion ...
+	XcodeArchiveMacVersion = "1.6.0"
+)
+
+const (
+	// XcodeTestMacID ...
+	XcodeTestMacID = "xcode-test-mac"
+	// XcodeTestMacVersion ...
+	XcodeTestMacVersion = "1.1.0"
+)
+
+const (
+	// CordovaArchiveID ...
+	CordovaArchiveID = "cordova-archive"
+	// CordovaArchiveVersion ...
+	CordovaArchiveVersion = "1.0.0"
+)
+
+const (
+	// IonicArchiveID ...
+	IonicArchiveID = "ionic-archive"
+	// IonicArchiveVersion ...
+	IonicArchiveVersion = "1.2.1"
+)
+
+const (
+	// GenerateCordovaBuildConfigID ...
+	GenerateCordovaBuildConfigID = "generate-cordova-build-configuration"
+	// GenerateCordovaBuildConfigVersion ...
+	GenerateCordovaBuildConfigVersion = "0.9.3"
+)
+
+const (
+	// JasmineTestRunnerID ...
+	JasmineTestRunnerID = "jasmine-runner"
+	// JasmineTestRunnerVersion ...
+	JasmineTestRunnerVersion = "0.9.0"
+)
+
+const (
+	// KarmaJasmineTestRunnerID ...
+	KarmaJasmineTestRunnerID = "karma-jasmine-runner"
+	// KarmaJasmineTestRunnerVersion ...
+	KarmaJasmineTestRunnerVersion = "0.9.1"
+)
+
+const (
+	// NpmID ...
+	NpmID = "npm"
+	// NpmVersion ...
+	NpmVersion = "0.9.0"
+)
