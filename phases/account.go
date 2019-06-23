b74800f5f0d26a1235bfd58119608c0bc86d6e26
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-06-23 15:18:57 +0200

    account phase

--- phases/account.go
+++ phases/account.go
@@ -1,105 +1,109 @@
 package phases
 
 import (
-	"bufio"
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
-	"strconv"
-	"strings"
 
 	"github.com/bitrise-io/go-utils/log"
+	"github.com/manifoldco/promptui"
 )
 
-type organizationData map[string]interface{}
-
-func isValid(choice int, limit int) bool {
-	return choice >= 1 && choice <= limit
+type organizationData struct {
+	Name string
+	Slug string
 }
 
-// Account returns the slug of the selected account. If the user selects
-// the personal account, the slug is empty.
-func Account(apiToken string) (string, error) {
+type OrganizationsRespone struct {
+	Data []organizationData
+}
+
+type MeData struct {
+	Username string
+}
+
+type MeResponse struct {
+	Data MeData
+}
+
+func fetchOrgs(apiToken string) (*OrganizationsRespone, error) {
 	req, err := http.NewRequest(http.MethodGet, "https://api.bitrise.io/v0.1/organizations", nil)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	req.Header.Set("Authorization", "token "+apiToken)
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	if res.StatusCode != 200 {
-		return "", fmt.Errorf("fetch orgs: %s", res.Status)
+		return nil, fmt.Errorf("server response: %s", res.Status)
 	}
 
-	var m struct {
-		Data []organizationData
-	}
-	if err := json.NewDecoder(res.Body).Decode(&m); err != nil {
-		return "", err
+	var orgs OrganizationsRespone
+	if err := json.NewDecoder(res.Body).Decode(&orgs); err != nil {
+		return nil, err
 	}
 
-	req, err = http.NewRequest(http.MethodGet, "https://api.bitrise.io/v0.1/me", nil)
+	return &orgs, nil
+}
+
+func fetchUser(apiToken string) (*MeResponse, error) {
+	req, err := http.NewRequest(http.MethodGet, "https://api.bitrise.io/v0.1/me", nil)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	req.Header.Set("Authorization", "token "+apiToken)
-	res, err = http.DefaultClient.Do(req)
+	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	if res.StatusCode != 200 {
-		return "", fmt.Errorf("fetch user: %s", res.Status)
+		return nil, fmt.Errorf("server response: %s", res.Status)
 	}
 
-	var u struct {
-		Data struct {
-			Username string
-		}
-	}
-	if err := json.NewDecoder(res.Body).Decode(&u); err != nil {
-		return "", err
+	var me MeResponse
+	if err := json.NewDecoder(res.Body).Decode(&me); err != nil {
+		return nil, err
 	}
 
-	options := append([]organizationData{organizationData{"name": u.Data.Username}}, m.Data...)
+	return &me, nil
+}
 
-	log.Infof("ACCOUNT OPTIONS")
-	log.Infof("===============")
-	for i, opt := range options {
-		log.Printf("%d) %s", i+1, opt["name"])
+// Account returns the slug of the selected account. If the user selects
+// the personal account, the slug is empty.
+func Account(apiToken string) (string, error) {
+	orgs, err := fetchOrgs(apiToken)
+	if err != nil {
+		return "", fmt.Errorf("fetch orgs for authenticated user: %s", err)
 	}
 
-	for {
-		log.Warnf("CHOOSE ACCOUNT: ")
-
-		reader := bufio.NewReader(os.Stdin)
-		input, err := reader.ReadString('\n')
-		if err != nil {
-			log.Errorf("error reading choice from stdin: %s", err)
-			continue
-		}
-
-		choice, err := strconv.Atoi(strings.TrimSpace(input))
-		if err != nil {
-			log.Errorf("error reading choice from stdin: %s", err)
-			continue
-		}
-		if !isValid(choice, len(options)) {
-			log.Errorf("invalid choice")
-			continue
-		}
-		if choice == 1 {
-			// own user selected
-			return "", nil
-		}
-
-		// organization selected
-		return options[choice-1]["slug"].(string), nil
+	user, err := fetchUser(apiToken)
+	if err != nil {
+		return "", fmt.Errorf("fetch authenticated user: %s", err)
 	}
+
+	accMap := map[string]string{}
+	items := []string{user.Data.Username}
+	for _, data := range orgs.Data {
+		accMap[data.Name] = data.Slug
+		items = append(items, data.Name)
+	}
+
+	log.Infof("CHOOSE ACCOUNT")
+	prompt := promptui.Select{
+		Label: "Select account to use",
+		Items: items,
+	}
+
+	_, acc, err := prompt.Run()
+	if err != nil {
+		return "", fmt.Errorf("scan user input: %s", err)
+	}
+
+	return accMap[acc], nil
 }
