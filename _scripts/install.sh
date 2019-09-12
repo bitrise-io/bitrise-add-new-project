35a21e3081c8ab7a286650a4307aed063b1311ce
Gödrei Krisztián <godreikrisztian@gmail.com>
2019-09-12 20:32:26 +0200

    initial install wrapper added

---
+++ _scripts/install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -e
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    --version)
+    VERSION="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --api-token)
+    API_TOKEN="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --org)
+    ORG="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --public)
+    PUBLIC="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+echo " => Creating a temporary directory for banp (bitrise-add-new-project) ..."
+temp_dir="$(mktemp -d -t banpXXXXXX)"
+bin_path="${temp_dir}/banp"
+
+version="0.2.1"
+if [ "${VERSION}" != "" ] ; then
+    version="${VERSION}"
+fi
+echo " => Downloading version: ${version}"
+
+download_url="https://github.com/bitrise-io/bitrise-add-new-project/releases/download/${version}/banp-Darwin-x86_64"
+echo " => Downloading banp from (${download_url}) to (${bin_path}) ..."
+curl -fL --progress-bar --output "${bin_path}" "${download_url}"
+echo " => Making it executable ..."
+chmod +x "${bin_path}"
+echo " => banp version: $(${bin_path} version)"
+echo " => Running banp ..."
+echo
+${bin_path} --api-token "${API_TOKEN}" --org "${ORG}" --public "${PUBLIC}"
