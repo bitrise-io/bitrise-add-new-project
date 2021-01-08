ab8bcf773b40554bb75c5dea3b57961d7c994757
lpusok <7979773+lpusok@users.noreply.github.com>
2021-01-08 16:40:46 +0100

    dependencies updated (#47)

--- vendor/github.com/bitrise-io/xcode-project/xcodeproj/xcodeproj.go
+++ vendor/github.com/bitrise-io/xcode-project/xcodeproj/xcodeproj.go
@@ -304,58 +304,49 @@ func Open(pth string) (XcodeProj, error) {
 		return XcodeProj{}, err
 	}
 
-	format, raw, objects, projectID, err := open(pth)
-	if err != nil {
-		return XcodeProj{}, err
-	}
-
-	p, err := parseProj(projectID, objects)
-	if err != nil {
-		return XcodeProj{}, err
-	}
-
-	return XcodeProj{
-		Proj:    p,
-		RawProj: raw,
-		Format:  format,
-		Path:    absPth,
-		Name:    strings.TrimSuffix(filepath.Base(absPth), filepath.Ext(absPth)),
-	}, nil
-}
-
-// open parse the provided .pbxprog file.
-// Returns the `raw` contents as a serialized.Object, the `objects` as serialized.Object and the PBXProject's `projectID` as string
-// If there was an error during the parsing it returns an error
-func open(absPth string) (format int, rawPbxProj serialized.Object, objects serialized.Object, projectID string, err error) {
 	pbxProjPth := filepath.Join(absPth, "project.pbxproj")
 
 	var b []byte
 	b, err = fileutil.ReadBytesFromFile(pbxProjPth)
 	if err != nil {
-		return
+		return XcodeProj{}, err
 	}
 
-	if format, err = plist.Unmarshal(b, &rawPbxProj); err != nil {
-		err = fmt.Errorf("failed to generate json from Pbxproj - error: %s", err)
-		return
-	}
-
-	objects, err = rawPbxProj.Object("objects")
+	p, err := parsePBXProjContent(b)
 	if err != nil {
-		return
+		return XcodeProj{}, err
 	}
 
+	p.Path = absPth
+	p.Name = strings.TrimSuffix(filepath.Base(absPth), filepath.Ext(absPth))
+
+	return *p, nil
+}
+
+func parsePBXProjContent(content []byte) (*XcodeProj, error) {
+	var rawPbxProj serialized.Object
+	format, err := plist.Unmarshal(content, &rawPbxProj)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal project.pbxproj: %s", err)
+	}
+
+	objects, err := rawPbxProj.Object("objects")
+	if err != nil {
+		return nil, err
+	}
+
+	var projectID string
 	for id := range objects {
 		var object serialized.Object
 		object, err = objects.Object(id)
 		if err != nil {
-			return
+			return nil, err
 		}
 
 		var objectISA string
 		objectISA, err = object.String("isa")
 		if err != nil {
-			return
+			return nil, err
 		}
 
 		if objectISA == "PBXProject" {
@@ -363,7 +354,21 @@ func open(absPth string) (format int, rawPbxProj serialized.Object, objects seri
 			break
 		}
 	}
-	return
+
+	if projectID == "" {
+		return nil, fmt.Errorf("failed to find PBXProject's id in project.pbxproj")
+	}
+
+	proj, err := parseProj(projectID, objects)
+	if err != nil {
+		return nil, err
+	}
+
+	return &XcodeProj{
+		Proj:    proj,
+		RawProj: rawPbxProj,
+		Format:  format,
+	}, nil
 }
 
 // IsXcodeProj ...
@@ -380,11 +385,6 @@ func (p *XcodeProj) ForceCodeSign(configuration, targetName, developmentTeam, co
 	target, ok := p.Proj.TargetByName(targetName)
 	if !ok {
 		return fmt.Errorf("failed to find target with name: %s", targetName)
-	}
-
-	targetAttributes, err := p.TargetAttributes()
-	if err != nil {
-		return fmt.Errorf("failed to get project's target attributes, error: %s", err)
 	}
 
 	buildConfigurationList, err := p.BuildConfigurationList(target.ID)
@@ -408,24 +408,33 @@ func (p *XcodeProj) ForceCodeSign(configuration, targetName, developmentTeam, co
 		return fmt.Errorf("failed to find buildConfiguration for configuration %s in the buildConfiguration list: %s", configuration, pretty.Object(buildConfigurations))
 	}
 
-	// Override TargetAttributes
-	if err = foreceCodeSignOnTargetAttributes(targetAttributes, target.ID, developmentTeam); err != nil {
-		return fmt.Errorf("failed to change code signing in target attributes, error: %s", err)
-	}
-
 	// Override BuildSettings
-	if err = foreceCodeSignOnBuildConfiguration(buildConfiguration, target.ID, developmentTeam, provisioningProfileUUID, codesignIdentity); err != nil {
+	if err = forceCodeSignOnBuildConfiguration(buildConfiguration, developmentTeam, provisioningProfileUUID, codesignIdentity); err != nil {
 		return fmt.Errorf("failed to change code signing in build settings, error: %s", err)
 	}
+
+	if targetAttributes, err := p.TargetAttributes(); err == nil {
+		// Override TargetAttributes
+		if err = forceCodeSignOnTargetAttributes(targetAttributes, target.ID, developmentTeam); err != nil {
+			return fmt.Errorf("failed to change code signing in target attributes, error: %s", err)
+		}
+	} else if !serialized.IsKeyNotFoundError(err) {
+		return fmt.Errorf("failed to get project's target attributes, error: %s", err)
+	}
+
 	return nil
 }
 
-// foreceCodeSignOnTargetAttributes sets the TargetAttributes for the provided targetID.
+// forceCodeSignOnTargetAttributes sets the TargetAttributes for the provided targetID.
 // **Overrides the ProvisioningStyle, developmentTeam and clears the DevelopmentTeamName in the provided `targetAttributes`!**
-func foreceCodeSignOnTargetAttributes(targetAttributes serialized.Object, targetID, developmentTeam string) error {
+func forceCodeSignOnTargetAttributes(targetAttributes serialized.Object, targetID, developmentTeam string) error {
 	targetAttribute, err := targetAttributes.Object(targetID)
 	if err != nil {
-		return fmt.Errorf("failed to get traget's (%s) attributes, error: %s", targetID, err)
+		// Skip projects not using target attributes
+		if serialized.IsKeyNotFoundError(err) {
+			return nil
+		}
+		return fmt.Errorf("failed to get target's (%s) attributes, error: %s", targetID, err)
 	}
 
 	targetAttribute["ProvisioningStyle"] = "Manual"
@@ -434,23 +443,41 @@ func foreceCodeSignOnTargetAttributes(targetAttributes serialized.Object, target
 	return nil
 }
 
-// foreceCodeSignOnBuildConfiguration sets the BuildSettings for the provided targetID.
-// **Overrides the CODE_SIGN_STYLE, DEVELOPMENT_TEAM, CODE_SIGN_IDENTITY, CODE_SIGN_IDENTITY[sdk=iphoneos\*], PROVISIONING_PROFILE, PROVISIONING_PROFILE[sdk=iphoneos\*] and clears the PROVISIONING_PROFILE_SPECIFIER in the provided `buildConfiguration`!**
-func foreceCodeSignOnBuildConfiguration(buildConfiguration serialized.Object, targetID, developmentTeam, provisioningProfileUUID, codesignIdentity string) error {
+// forceCodeSignOnBuildConfiguration sets the BuildSettings for the provided build configuration.
+// **Overrides the CODE_SIGN_STYLE, DEVELOPMENT_TEAM, CODE_SIGN_IDENTITY, PROVISIONING_PROFILE
+// and clears the PROVISIONING_PROFILE_SPECIFIER in the provided `buildConfiguration`,
+// each modification also applies for the sdk specific settings too (CODE_SIGN_IDENTITY[sdk=iphoneos*])!**
+func forceCodeSignOnBuildConfiguration(buildConfiguration serialized.Object, developmentTeam, provisioningProfileUUID, codesignIdentity string) error {
 	buildSettings, err := buildConfiguration.Object("buildSettings")
 	if err != nil {
 		return fmt.Errorf("failed to get buildSettings of buildConfiguration (%s), error: %s", pretty.Object(buildConfiguration), err)
 	}
 
-	buildSettings["CODE_SIGN_STYLE"] = "Manual"
-	buildSettings["DEVELOPMENT_TEAM"] = developmentTeam
-	buildSettings["CODE_SIGN_IDENTITY"] = codesignIdentity
-	buildSettings["CODE_SIGN_IDENTITY[sdk=iphoneos*]"] = codesignIdentity
-	buildSettings["PROVISIONING_PROFILE_SPECIFIER"] = ""
-	buildSettings["PROVISIONING_PROFILE"] = provisioningProfileUUID
-	buildSettings["PROVISIONING_PROFILE[sdk=iphoneos*]"] = provisioningProfileUUID
+	forceAttributes := map[string]string{
+		"CODE_SIGN_STYLE":                "Manual",
+		"DEVELOPMENT_TEAM":               developmentTeam,
+		"CODE_SIGN_IDENTITY":             codesignIdentity,
+		"PROVISIONING_PROFILE_SPECIFIER": "",
+		"PROVISIONING_PROFILE":           provisioningProfileUUID,
+	}
+	for key, value := range forceAttributes {
+		writeAttributeForAllSDKs(buildSettings, key, value)
+	}
 
 	return nil
+}
+
+func writeAttributeForAllSDKs(buildSettings serialized.Object, newKey string, newValue string) {
+	buildSettings[newKey] = newValue
+
+	// override specific build setting if any: https://stackoverflow.com/a/5382708/5842489
+	// Example: CODE_SIGN_IDENTITY[sdk=iphoneos*]
+	matcher := regexp.MustCompile(fmt.Sprintf(`^%s\[sdk=.*\]$`, regexp.QuoteMeta(newKey)))
+	for oldKey := range buildSettings {
+		if matcher.Match([]byte(oldKey)) {
+			buildSettings[oldKey] = newValue
+		}
+	}
 }
 
 // Save the XcodeProj
